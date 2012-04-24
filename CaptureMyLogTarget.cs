eeec3fefde029ee935b64b621fa6011ee89f2999
Marius Ion <marius.ion85@gmail.com>
2012-04-24 23:54:41 +0300

    Added nlog appender and binaries

---
+++ CaptureMyLogTarget.cs
@@ -0,0 +1,33 @@
+﻿using CaptureMyLog.Framework;
+using NLog;
+using NLog.Targets;
+
+namespace CaptureMyLog.NLog
+{
+    [Target("CaptureMyLogTarget")]
+    public class CaptureMyLogTarget : TargetWithLayout
+    {
+        #region Properties
+
+        public string ApplicationKey { get; set; }
+        public string OwnerKey { get; set; }
+        public string DeviceKey { get; set; }
+        public bool IsTakingPictureEnabled { get; set; }
+
+        #endregion Properties
+
+        #region Overriden Members
+
+        protected override void Write(LogEventInfo logEvent)
+        {
+            byte[] picture = null;
+            if (this.IsTakingPictureEnabled)
+                picture = Helpers.CaptureScreenAsJpeg();
+
+            //LogDataWrapper.LogNewDataAsync(this.ApplicationKey, this.DeviceKey, this.OwnerKey, logEvent.FormattedMessage, logEvent.Level.Name, picture);
+            LogDataWrapper.LogNewData(this.ApplicationKey, this.DeviceKey, this.OwnerKey, logEvent.FormattedMessage, logEvent.Level.Name, picture);
+        }
+
+        #endregion Overriden Members
+    }
+}
